f937639c4acf8b2ce7f0516e37e2d61f18058970
brianngumbau <brianngumbau9075@gmail.com>
2026-03-26 12:14:29 +0300

    added '0.0.0.0'to server.js

--- server.js
+++ server.js
@@ -62,6 +62,6 @@ app.get('/', (req, res) => {
 // 8. PRODUCTION LISTEN LOGIC
 // Render assigns a dynamic port. Binding to '0.0.0.0' is mandatory for Render to route traffic.
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
